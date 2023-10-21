0167a482d00e116dad09d1b4ca42329cebc5c3e0
marat2509 <93652988+marat2509@users.noreply.github.com>
2023-10-21 23:59:34 +0300

    updater: fix 2

--- updater.py
+++ updater.py
@@ -24,15 +24,15 @@ for repo in repos:
     if clone_cmd.returncode == 0:
         logger.info(f"Successfully cloned {source_repo} to {mirror_dir}")
     else:
-        logger.error(f"Error in cloning {source_repo} to {mirror_dir}, status code: {str(clone_cmd.returncode)}:\n" +
+        logger.error(f"Error while cloning {source_repo} to {repo_dir}, status code: {str(clone_cmd.returncode)}:\n" +
                      f"- stdout:\n{clone_cmd.stdout}\n- stderr:\n{clone_cmd.stderr}")
         continue
 
-    logging.info(f"Pushing {source_dir} to {destination_dir}...")
+    logging.info(f"Pushing {destination_repo} ({repo_dir}) to {destination_dir}...")
     push_cmd = sp.run(["git", "--git-dir", repo_dir, "push", "--mirror", destination_repo], capture_output=True, text=True)
     if push_cmd.returncode == 0:
-        logger.success(f"Successfully pushed {source_repo} to {mirror_dir}")
+        logger.success(f"Successfully pushed {source_repo} to {repo_dir}")
     else:
-        logger.error(f"Error in pushing {source_repo} to {mirror_dir}, status code: {str(push_cmd.returncode)}:\n"+
+        logger.error(f"Error while pushing {source_repo} to {repo_dir}, status code: {str(push_cmd.returncode)}:\n"+
                      f"- stdout:\n{push_cmd.stdout}\n- stderr:\n{push_cmd.stderr}")
         continue
