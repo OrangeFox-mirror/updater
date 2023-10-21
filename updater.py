e0dfd45bebf762956b3ca898f922dbb196a5d651
marat2509 <93652988+marat2509@users.noreply.github.com>
2023-10-22 00:08:11 +0300

    updater: fix 5

--- updater.py
+++ updater.py
@@ -7,7 +7,10 @@ with open("repos.json", "r", encoding="utf-8") as repos_file:
     repos = json.load(repos_file)
     logger.info("Imported repo list")
 
+print("\n")
+
 for repo in repos:
+    print("======================")
     source_repo = repo["source"]
     destination_repo = repo["destination"]
     repo_dir = destination_repo.split("/"
@@ -22,17 +25,21 @@ for repo in repos:
                         capture_output=True,
                         text=True)
     if clone_cmd.returncode == 0:
-        logger.info(f"Successfully cloned {source_repo} to {repo_dir}")
+        logger.success(f"Successfully cloned {source_repo} to {repo_dir}")
     else:
         logger.error(f"Error while cloning {source_repo} to {repo_dir}, status code: {str(clone_cmd.returncode)}:\n" +
                      f"- stdout:\n{clone_cmd.stdout}\n- stderr:\n{clone_cmd.stderr}")
+        print("======================\n")
         continue
 
-    logging.info(f"Pushing {destination_repo} ({repo_dir}) to {destination_dir}...")
+    logger.info(f"Pushing {destination_repo} ({repo_dir}) to {destination_dir}...")
     push_cmd = sp.run(["git", "--git-dir", repo_dir, "push", "--mirror", destination_repo], capture_output=True, text=True)
     if push_cmd.returncode == 0:
         logger.success(f"Successfully pushed {source_repo} to {destination_repo}")
     else:
         logger.error(f"Error while pushing {source_repo} to {destination_repo}, status code: {str(push_cmd.returncode)}:\n"+
                      f"- stdout:\n{push_cmd.stdout}\n- stderr:\n{push_cmd.stderr}")
+        print("======================\n")
         continue
+
+    print("======================\n")
