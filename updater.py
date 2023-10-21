1d1a3350d8e2edd6d3f25511d7df49ce1aeb98d5
marat2509 <93652988+marat2509@users.noreply.github.com>
2023-10-21 23:52:22 +0300

    update: many improvments and fixes

---
+++ updater.py
@@ -0,0 +1,38 @@
+import subprocess as sp
+import json
+import os
+from loguru import logger
+
+with open("repos.json", "r", encoding="utf-8") as repos_file:
+    repos = json.load(repos_file)
+    logger.info("Imported repo list")
+
+for repo in repos:
+    source_repo = repo["source"]
+    destination_repo = repo["destination"]
+    repo_dir = destination_repo.split("/"
+                                        )[-1].replace(
+                                                ".git/", ".git"
+                                            ).replace(
+                                                ".git", ""
+                                            )
+    logger.info(f"Clonning {source_repo} to {mirror_dir}...")
+    clone_cmd = sp.run(["git", "clone", "--mirror",
+                        destination_repo, repo_dir],
+                        capture_output=True,
+                        text=True)
+    if clone_cmd.returncode == 0:
+        logger.info(f"Successfully cloned {source_repo} to {mirror_dir}")
+    else:
+        logger.error(f"Error in cloning {source_repo} to {mirror_dir}, status code: {str(clone_cmd.returncode)}:
+                     - stdout:\n{clone_cmd.stdout}\n- stderr:\n{clone_cmd.stderr}")
+        continue
+
+    logging.info(f"Pushing {source_dir} to {destination_dir}...")
+    push_cmd = sp.run(["git", "--git-dir", repo_dir, "push", "--mirror", destination_repo], capture_output=True, text=True)
+    if push_cmd.returncode == 0:
+        logger.success(f"Successfully pushed {source_repo} to {mirror_dir}")
+    else:
+        logger.error(f"Error in pushing {source_repo} to {mirror_dir}, status code: {str(push_cmd.returncode)}:
+                     - stdout:\n{push_cmd.stdout}\n- stderr:\n{push_cmd.stderr}")
+        continue
